3769d1cd130a0708f1f9d57ccb9e9d0a709b212e
Leonel <llopez@bananacode.co>
2022-03-03 15:46:12 -0600

    ajuste navigator bar para movil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,41 +27,46 @@
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
-                    <ul class="flex">
-                        <li><a class="no-underline text-black mr-4" target="_blank"
-                                href="https://www.handsonlesco.com/">¿Quienés somos?</a></li>
-                        <li><a class="no-underline  text-black mr-4"" target=" _blank"
-                                href="https://www.handsonlesco.com/blog/">Blog</a></li>
-                        <li><a class="no-underline  text-black mr-4" target="_blank"
-                                href="https://www.handsonlesco.com/contactanos/">Contacto</a></li>
-                        <li><a class="py-13 pt-15  mr-4 text-black" target="_blank"
-                                href="https://www.handsonlesco.com/cursos/">Cursos</a></li>
-
-                    </ul>
-                    <!-- mobile menu -->
-                    <div class="hidden mobile-menu">
-                        <ul class="">
-                            <li class="active"><a href="index.html"
-                                    class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-                            <li><a href="#services"
-                                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a>
-                            </li>
-                            <li><a href="#about"
-                                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a>
-                            </li>
-                            <li><a href="#contact"
-                                    class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact
-                                    Us</a></li>
+                    <div class="hidden md:flex items-center space-x-1">
+                        <ul class="flex">
+                          <li><a class="no-underline text-black mr-4" target="_blank"
+                            href="https://www.handsonlesco.com/">¿Quienés somos?</a></li>
+                          <li><a class="no-underline  text-black mr-4" target="_blank"
+                            href="https://www.handsonlesco.com/blog/">Blog</a></li>
+                       <li><a class="no-underline  text-black mr-4" target="_blank"
+                            href="https://www.handsonlesco.com/contactanos/">Contacto</a> </li>                     
+                        <li> <a class="py-13 pt-15  mr-4 text-black" target="_blank"
+                            href="https://www.handsonlesco.com/cursos/">Cursos</a></li>
                         </ul>
                     </div>
-                    <script>
-                        const btn = document.querySelector("button.mobile-menu-button");
-                        const menu = document.querySelector(".mobile-menu");
-
-                        btn.addEventListener("click", () => {
-                            menu.classList.toggle("hidden");
-                        });
-                    </script>
+                    <!-- Mobile menu button -->
+                    <div class="md:hidden flex items-center">
+                        <button class="outline-none mobile-menu-button">
+                            <svg class=" w-6 h-6 text-gray-500 hover:text-blue-500 " x-show="!showMenu" fill="none"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </button>
+                    </div>
+                        <!-- mobile menu -->
+                        <div class="hidden mobile-menu">
+                            <ul class="">
+                                <li class="active"><a href="https://www.handsonlesco.com/"
+                                        class="block text-sm px-2 py-4 text-black hover:bg-blue-500 font-semibold">¿Quienés somos?</a>
+                                </li>
+                                <li><a href="https://www.handsonlesco.com/blog/"
+                                        class="block text-sm px-2 py-4 text-black  hover:bg-blue-500 transition duration-300">Blog</a>
+                                </li>
+                                <li><a href="https://www.handsonlesco.com/contactanos/"
+                                        class="block text-sm px-2 py-4 text-black hover:bg-blue-500 transition duration-300">Contacto</a>
+                                </li>
+                                <li><a href="https://www.handsonlesco.com/cursos/"
+                                        class="block text-sm px-2 py-4 text-black hover:bg-blue-500 transition duration-300">Cursos
+                                        Us</a></li>
+                            </ul>
+                        </div>
+                   
                 </nav>
             </div>
         </header>
@@ -69,5 +74,12 @@
         @yield('content')
     </div>
 </body>
+<script>
+    const btn = document.querySelector("button.mobile-menu-button");
+    const menu = document.querySelector(".mobile-menu");
 
+    btn.addEventListener("click", () => {
+        menu.classList.toggle("hidden");
+    });
+</script>
 </html>
