c5a90a1e1eb922fdbfed920acc1fd0912d5400de
Leonel <llopez@bananacode.co>
2022-04-19 11:35:34 -0600

    ajuste de restablecer contrasena y lang es

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+ <img src="https://handsonlesco.com/wp-content/uploads/2020/10/HANDSON_HEADER_LOGO.png" class="logo" alt="Hands On Logo">
+</a>
+</td>
+</tr>
